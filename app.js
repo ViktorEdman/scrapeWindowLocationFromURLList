bb3f48ecf4c7e744a7249064333bb69b33638c24
ViktorEdman <viktor@edman.se>
2020-07-02 10:48:22 +0200

    added CLI

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const {exit} = require('process');
 const Sitemapper = require('sitemapper');
 const sitemap = new Sitemapper();
 
+const args = process.argv.slice(2);
+const urlToMap = args[0];
+const fileToWrite = args[1];
+
 async function grabRealUrl(url) {
     const browser = await puppeteer.launch({
         args: ['--no-sandbox']
@@ -24,16 +28,33 @@ async function grabRealUrl(url) {
     }
 }
 
-const siteMapUrl = "https://viktor5.starwebserver.se/sitemap-categories.xml";
-const outputFilename = "301.csv"
-const ws = fs.createWriteStream(outputFilename);
 
-sitemap.fetch(siteMapUrl).then(async function (sites) {
-    console.log(sites.sites);
-    process.setMaxListeners(sites.sites.length);
-    const requestArray = await Promise.all(sites.sites.map(grabRealUrl));
-    fastcsv
-        .write(requestArray, { headers: true })
-        .pipe(ws);
-    console.log('Wrote to '+outputFilename);
-});
+function mapSite(siteMapUrl, filename) {
+    const ws = fs.createWriteStream(filename);
+    sitemap.fetch(siteMapUrl).then(async function (sites) {
+        console.log(sites.sites);
+        process.setMaxListeners(sites.sites.length);
+        const requestArray = await Promise.all(sites.sites.map(grabRealUrl));
+        fastcsv
+            .write(requestArray, { headers: true })
+            .pipe(ws);
+        console.log('Wrote to ' + filename);
+    })
+};
+
+
+if (urlToMap === undefined || fileToWrite === undefined) {
+    console.log('No args detected.');
+    console.log('Correct usage is "node app.js [sitemap-url] [output-file].');
+    console.log('Please try again!');
+    process.exit(1);
+} else {
+    console.log('Sitemap URL: ' + urlToMap);
+    console.log('Output file: ' + fileToWrite);
+    mapSite(urlToMap, fileToWrite);
+}
+
+
+
+
+
---
+++ argstest.js
@@ -0,0 +1,7 @@
+const args = process.argv.slice(2);
+
+const sitemapURL = args[0];
+const outputFile = args[1];
+
+console.log('Sitemap URL: '+sitemapURL);
+console.log('Output file: '+outputFile);
