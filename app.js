e4618f6e0dc95eb2c9b0e60fbcb91bf5b3536fe4
ViktorEdman <viktor@edman.se>
2020-07-02 09:35:20 +0200

    restructured code

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const fastcsv = require('fast-csv');
 const fs = require('fs');
-const ws = fs.createWriteStream('301.csv');
 const puppeteer = require('puppeteer');
 const {exit} = require('process');
 const Sitemapper = require('sitemapper');
 const sitemap = new Sitemapper();
 
+async function grabRealUrl(url) {
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox']
+    })
+    try {
+        let page = await browser.newPage();
+        await page.goto(url);
+        let grabbedUrl = page.url();
+        return {
+            fromUrl: grabbedUrl,
+            toUrl: url
+        };
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await browser.close();
+    }
+}
+
 const siteMapUrl = "https://viktor5.starwebserver.se/sitemap-categories.xml";
+const outputFilename = fs.createWriteStream('301.csv');
 
 sitemap.fetch(siteMapUrl).then(async function (sites) {
     console.log(sites.sites);
@@ -14,21 +33,8 @@ sitemap.fetch(siteMapUrl).then(async function (sites) {
     const requestArray = await Promise.all(sites.sites.map(grabRealUrl));
     fastcsv
         .write(requestArray, { headers: true })
-        .pipe(ws);
+        .pipe(outputFilename);
 });
 
 
-async function grabRealUrl(url) {
-    const browser = await puppeteer.launch({args: ['--no-sandbox']})
-    try {
-        let page = await browser.newPage();
-        await page.goto(url);
-        let grabbedUrl = page.url();
-        return {fromUrl: grabbedUrl, toUrl: url};
-    } catch (error) {
-        console.error(error);
-    } finally {
-        await browser.close();
-    }
-}
 
